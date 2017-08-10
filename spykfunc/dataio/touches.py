ec0739e9de6da83408b980cc987a11c1e48eb525
Fernando Pereira <fernando.pereira@epfl.ch>
2017-08-10 17:38:22 +0200

    Updating touch reader to support split touch files

--- spykfunc/dataio/touches.py
+++ spykfunc/dataio/touches.py
@@ -1,5 +1,11 @@
+from __future__ import print_function
+import sys
 from lazy_property import LazyProperty
 import numpy as np
+from abc import ABCMeta, abstractproperty
+from future.utils import with_metaclass
+import itertools
+import os.path
 from ._caching import CachedDataset, _DataSet
 import logging
 
@@ -10,67 +16,139 @@ class NEURON_STATS_F:
     binaryOffset = 2
 
 
-# Data types
-_header_dtype = np.dtype([
-    ("architectureIdentifier", np.double),
-    ("numberOfNeurons", np.longlong),
-    ("gitVersion", "S16")
-], align=True)
+class TouchInfo_Interface(with_metaclass(ABCMeta, object)):
+    @abstractproperty
+    def header(self):
+        pass
 
-_neuron_touches_dtype = np.dtype([
-    ("neuronID", np.int32),
-    ("touchesCount", np.uint32),
-    ("binaryOffset", np.longlong)
-], align=True)
+    @abstractproperty
+    def neuron_stats(self):
+        pass
 
-_touches_dtype = np.dtype([
-    ("pre_ids", np.int32, 3),
-    ("post_ids", np.int32, 3),
-    ("branch_order", np.int32),
-    ("distances", np.float32, 3),
-], align=True)
+    @abstractproperty
+    def touch_count(self):
+        pass
+
+    @abstractproperty
+    def touches(self):
+        pass
 
 
-class TouchInfo(object):
+class TouchInfo(TouchInfo_Interface):
+    def __new__(cls, files):
+        if isinstance(files, str):
+            return _TouchInfo(files)
+        else:
+            return object.__new__(TouchInfo)
+
+    def __init__(self, files):
+        self._touch_infos = [_TouchInfo(f) for f in files]
+
+    @LazyProperty
+    def header(self):
+        glob_header = self._touch_infos[0].header.copy()
+        glob_header.numberOfNeurons = sum(ti.header.numberOfNeurons
+                                          for ti in self._touch_infos)
+        return glob_header
+
+    @property
+    def neuron_stats(self):
+        return itertools.chain(*[info.neuron_stats for info in self._touch_infos])
+
+    @property
+    def touch_count(self):
+        return sum(info.touch_count for info in self._touch_infos)
+
+    @property
+    def touches(self):
+        return itertools.chain(*[info.touches for info in self._touch_infos])
+
+
+class _TouchInfo(TouchInfo_Interface):
+    """
+    Reader of Binary Neuron-Touches info
+    """
+    _header_dtype = np.dtype([
+        ("architectureIdentifier", np.double),
+        ("numberOfNeurons", np.longlong),
+        ("gitVersion", "S16")
+    ], align=True)
+
+    _neuron_touches_dtype = np.dtype([
+        ("neuronID", np.int32),
+        ("touchesCount", np.uint32),
+        ("binaryOffset", np.longlong)
+    ], align=True)
+
+    _neuron_touches_dtype_reversed = _neuron_touches_dtype.newbyteorder()
+
     def __init__(self, neuron_touches_file):
         # type: (str) -> None
         self._neuron_file = neuron_touches_file
         _last_dot = neuron_touches_file.rfind('.')
         self.touches_file = neuron_touches_file[:_last_dot] + 'Data' + neuron_touches_file[_last_dot:]
+        self._header = None
+        self._byte_swap = None
 
-    @LazyProperty
-    def _neuron_header_stats(self):
-        neuron_f = open(self._neuron_file)
+    def _read_header(self, f_handler):
         try:
-            header = np.rec.fromfile(neuron_f, dtype=_header_dtype, aligned=True, shape=1)[0]
+            header = np.rec.fromfile(f_handler, dtype=_TouchInfo._header_dtype, aligned=True, shape=1)[0]
         except:
             logging.fatal("Could not read header record from touches")
             return
+
         self._byte_swap = not np.equal(header.architectureIdentifier, np.double(1.001))
-        return header, np.fromfile(neuron_f, dtype=_neuron_touches_dtype)
+        if self._byte_swap:
+            header.numberOfNeurons = header.numberOfNeurons.byteswap()
+            self._neuron_touches_dtype = _TouchInfo._neuron_touches_dtype_reversed
+        # cache
+        self._header = header
+        return header
+
+    @property
+    def _neuron_stats(self):
+        print("Loading '{}'. Please wait...".format(os.path.basename(self._neuron_file)), end="\r")
+        sys.stdout.flush()
+        with open(self._neuron_file) as neuron_f:
+            _ = self._read_header(neuron_f)
+            info = np.fromfile(neuron_f, dtype=self._neuron_touches_dtype)
+        print("                                                                     ", end="\r")  # Clear
+        return info
 
     @property
     def header(self):
-        return self._neuron_header_stats[0]
+        return self._header or self._read_header(open(self._neuron_file))
 
     @property
     def neuron_stats(self):
-        return self._neuron_header_stats[1]
+        return self._neuron_stats
 
     @LazyProperty
     def touch_count(self):
-        return sum(stats[1] for stats in self.neuron_stats)
+        return int(self.neuron_stats['touchesCount'].sum())
 
     @LazyProperty
     def touches(self):
-        print("WARNING: TouchInfo.touches is lazily evaluated, returning an iterator."
+        print("WARNING: TouchInfo.touches is lazily evaluated, returning an iterator.\n"
               "         Please select a small region and/or avoid converting to a full array")
+        _ = self.header  # Init endianness if needed
         return CachedDataset(_Touches(self))
 
 
 # Raw accecs of touches
 # Do not use directly, only implemented to interface to CachedDataset
 class _Touches(_DataSet):
+    """
+    Reader for Binary touches
+    """
+    _touches_dtype = np.dtype([
+        ("pre_ids", np.int32, 3),
+        ("post_ids", np.int32, 3),
+        ("branch_order", np.int32),
+        ("distances", np.float32, 3),
+    ], align=True)
+    _touches_dtype_reversed = _touches_dtype.newbyteorder()
+
     _SIZEOF_TOUCH = _touches_dtype.itemsize
 
     def __init__(self, touch_info):
@@ -78,6 +156,8 @@ class _Touches(_DataSet):
         self._neuron_stats = touch_info.neuron_stats
         self._length = touch_info.touch_count
         self._touch_f = open(touch_info.touches_file)
+        self._touches_dtype = _Touches._touches_dtype if not touch_info._byte_swap\
+            else _Touches._touches_dtype_reversed
 
     def __del__(self):
         self._touch_f.close()
@@ -92,14 +172,14 @@ class _Touches(_DataSet):
             offset = index
             count = 1
         self._touch_f.seek(offset * self._SIZEOF_TOUCH)
-        return np.fromfile(self._touch_f, dtype=_touches_dtype, count=count)
+        return np.fromfile(self._touch_f, dtype=self._touches_dtype, count=count)
 
     def __setitem__(self, key, value):
         raise TypeError("Read-only structure")
 
     @property
     def dtype(self):
-        return _touches_dtype
+        return self._touches_dtype
 
     @property
     def shape(self):
