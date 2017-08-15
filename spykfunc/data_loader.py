Fixing all exchanges of neuron info, by broadcasting

spykfunc/data_loader.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, absolute_import
 import os
 
+from pyspark.sql import functions as F
 from .definitions import MType
 from .dataio.cppneuron import NeuronData
 from . import schema
@@ -55,22 +56,31 @@ class NeuronDataSpark(NeuronData):
         logger.info("Total neurons: %d", n_neurons)
         logger.debug("Partitions: %d", total_parts)
 
-        # Initial RDD has only the range objects
-        neuronRDD = self._sc.parallelize(range(total_parts), total_parts)
 
-        # LOAD neurons in parallel
-        name_accu = self._sc.accumulator({}, DictAccum())
-        neuronRDD = neuronRDD.flatMap(
-            neuron_loader_gen(NeuronData, self._loader.__class__, self._loader.get_params(), n_neurons, total_parts, name_accu,
-                              self.mtypeVec))
+        if self._spark.sql("show tables like 'neuronDF'").collect():
+            self.neuronDF = F.broadcast(self._spark.table("neuronDF").cache())
 
-        # Create DF
-        logger.info("Creating data frame...")
-        self.neuronDF = self._spark.createDataFrame(neuronRDD, schema.NEURON_SCHEMA).cache()
+        else:
+            # Initial RDD has only the range objects
+            neuronRDD = self._sc.parallelize(range(total_parts), total_parts)
 
-        # Evaluate to get NameMap
-        logger.info("Total: %d", self.neuronDF.count())
-        self.set_name_map(name_accu.value)
+            # LOAD neurons in parallel
+            name_accu = self._sc.accumulator({}, DictAccum())
+            neuronRDD = neuronRDD.flatMap(
+                neuron_loader_gen(NeuronData, self._loader.__class__, self._loader.get_params(), n_neurons, total_parts, name_accu,
+                                  self.mtypeVec))
+
+            # Create DF
+            logger.info("Creating data frame...")
+            # Mark as "broadcastable" and cache
+            self.neuronDF = F.broadcast(self._spark.createDataFrame(neuronRDD, schema.NEURON_SCHEMA)).cache()            
+
+            # Evaluate to get NameMap
+            logger.info("Total: %d", self.neuronDF.count())
+            self.set_name_map(name_accu.value)
+
+            neuronDF.write.saveAsTable("neuronDF")
+        
 
     # ---
     def _load_h5_morphologies(self, names, filter=None, total_parts=128):
@@ -98,8 +108,6 @@ class NeuronDataSpark(NeuronData):
             for f in files[1:]:
                 self.touchDF.union(self.load_touch_parquet(f))
 
-        # Caching everything might be evil
-        # self.touchDF = self.touchDF.cache()
         return self.touchDF
 
     # ---

spykfunc/filters.py
@@ -12,7 +12,7 @@ if False: from .recipe import ConnectivityPathRule   # NOQA
 logger = get_logger(__name__)
 
 # Control variable that outputs intermediate calculations, and makes processing slower
-_DEBUG = False
+_DEBUG = True
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -28,9 +28,11 @@ class BoutonDistanceFilter(DataSetOperation):
         self.synapse_classes_indexes = {syn: index for index, syn in enumerate(synapse_classes_by_index)}
 
     def apply(self, neuronG, *args, **kw):
+        # neuronDF = F.broadcast(neuronG.vertices)
         neuronDF = neuronG.vertices
         touchDF = neuronG.edges
-
+        
+        # Use broadcast of Neuron version
         newTouches = touchDF.alias("t").join(neuronDF.alias("n"), neuronDF.id == touchDF.dst) \
             .where("(t.distance_soma >= %f AND n.syn_class_index = %d) OR "
                    "(t.distance_soma >= %f AND n.syn_class_index = %d)" % (
@@ -126,9 +128,9 @@ class ReduceAndCut(DataSetOperation):
     # ---
     def apply(self, neuronG, *args, **kw):
 
-        params_df = self.compute_reduce_cut_params() \
-            .select(self._make_assoc_expr("n1_morpho", "n2_morpho"), "*")
-
+        params_df = F.broadcast(self.compute_reduce_cut_params() \
+            .select(self._make_assoc_expr("n1_morpho", "n2_morpho"), "*"))
+            
         # Flatten GraphFrame and include morpho>morpho row
         full_touches = neuronG.find("(n1)-[t]->(n2)") \
             .select(self._make_assoc_expr("n1.morphology", "n2.morphology"), "*")
@@ -154,6 +156,7 @@ class ReduceAndCut(DataSetOperation):
     # ---
     def compute_reduce_cut_params(self):
         # First obtain the morpho-morpho stats dataframe
+        
         mtype_stats = self.stats.mtype_touch_stats
 
         # param create udf
@@ -184,11 +187,7 @@ class ReduceAndCut(DataSetOperation):
     def apply_cut(reduced_touches, params_df):
         """ Apply cut filter
         """
-
-        # Used few times, with large mem should profit from caching
-        # reduced_touches = reduced_touches.cache()
-
-        params_df = params_df.withColumn("sigma", params_df.pMu_A / 4)
+        params_df = F.broadcast(params_df.withColumn("sigma", params_df.pMu_A / 4))
 
         post_neuron_touch_counts = (
             reduced_touches
@@ -208,8 +207,9 @@ class ReduceAndCut(DataSetOperation):
             .where(F.col("rand") < F.col("survivalRate"))
             .select(F.col("morpho_assoc").alias("m"), F.col("post_neuron_id"))
         )
+        
         # A small DF which is required entirely by all workers
-        F.broadcast(shall_not_cut)
+        shall_not_cut = F.broadcast(shall_not_cut.cache())
 
         cut_touches = (
             reduced_touches

spykfunc/stats.py
@@ -32,7 +32,7 @@ class NeuronStats(object):
     def update_touch_graph_source(self, touch_GF, skip_update=False):
         self._touch_graph_frame = touch_GF
         # Define the DF of neurons-neurons counts, with morphology names
-        self.neurons_touch_counts = self._build_neurons_touch_counts(touch_GF)
+        self.neurons_touch_counts = self._build_neurons_touch_counts(touch_GF).cache()
         if not skip_update:
             self._prev_gf = self._touch_graph_frame
             self.total_neurons = self._touch_graph_frame.vertices.count()
@@ -64,20 +64,22 @@ class NeuronStats(object):
             F.sum(col("count")).alias("total_touches"),
             F.count(col("*")).alias("total_connections"))
 
-        return morpho_touches_conns.withColumn("average_touches_conn",
-                                               morpho_touches_conns.total_touches / morpho_touches_conns.total_connections)
+        morpho_touches_conns = morpho_touches_conns.withColumn(
+            "average_touches_conn",
+            morpho_touches_conns.total_touches / morpho_touches_conns.total_connections)
+            
+        return morpho_touches_conns.cache()
 
     @staticmethod
     def _build_neurons_touch_counts(neuronG):
         """ Counts the total touches between morphologies and neurons.
-            It is an important calculation, not so large, so we cache
         """
         return neuronG.find("(n1)-[t]->(n2)").groupBy(
             col("n1.morphology").alias("n1_morpho"),
             col("n2.morphology").alias("n2_morpho"),
             col("n1.id").alias("n1_id"),
-            col("n2.id").alias("n2_id")
-        ).count().cache()
+            col("n2.id").alias("n2_id"),
+        ).count()
 
 
 class MTYPE_STATS_FIELDS: