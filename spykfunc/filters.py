86a43fd42af4686953071756a21a0d42c4852f8d
Fernando Pereira <fernando.pereira@epfl.ch>
2017-10-12 14:31:48 +0200

    cleanup

--- spykfunc/filters.py
+++ spykfunc/filters.py
@@ -3,7 +3,6 @@ from __future__ import division
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
 from pyspark import StorageLevel
-from math import exp
 from .definitions import CellClass
 from ._filtering import DataSetOperation
 from .utils import get_logger
--- tests/test_functionalizer.py
+++ tests/test_functionalizer.py
@@ -6,7 +6,7 @@ args = (
     path.join(BASE_DIR, "recipe/builderRecipeAllPathways.xml"),  # recipe_file
     path.join(BASE_DIR, "circuits/circuit.mvd3"),                # mvd_file
     path.join(BASE_DIR, "morphologies/h5"),                      # morpho_dir
-    path.join(BASE_DIR, "BlueDetector_output/touches.0")         # touch_files
+    path.join(BASE_DIR, "BlueDetector_output/touches.0"),        # touch_files
 )
 
 if __name__ == "__main__":
