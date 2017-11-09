6072a5211bc17ee615da1b149991d906afa88311
Fernando Pereira <fernando.pereira@epfl.ch>
2017-11-09 11:51:15 +0100

    nnr_compare: fixed bug and improved report

--- utils/nrn_compare.py
+++ utils/nrn_compare.py
@@ -1,12 +1,23 @@
 """
-Program which takes two outputs of functionalizer and analyzes their similarity
+nrn_compare: takes two outputs of functionalizer and analyzes their similarity
+
+Usage:
+  nrn_compare summary <file1> <file2>
+  nrn_compare -h
+
+Options:
+  -h                Show help
+  -vv               Verbose mode (-v for info, -vv for debug)
 """
+
 from __future__ import print_function
 import h5py
 import numpy
 from docopt import docopt
 import logging
 logging.basicConfig(level=logging.INFO)
+from pprint import pformat
+
 
 def check_summary(summary1, summary2):
     f1 = h5py.File(summary1, mode="r")
@@ -22,27 +33,33 @@ def check_summary(summary1, summary2):
 
     ds_count = len(f1_keyset)
     progress_each = ds_count // min(100, round(ds_count / 500.0, 0))
+    probs = []
 
     for i, ds_name in enumerate(f1_keyset - {"info"}):
         logging.debug("Dataset: %s", ds_name)
-        ds1 = f1[ds_name]
-        ds2 = f1[ds_name]
-        numpy.testing.assert_array_equal(ds1[:], ds2[:], "Error with datasets {}".format(ds_name))
+        ds1 = f1[ds_name][:]
+        ds2 = f2[ds_name][:]
+        if not numpy.array_equal(ds1, ds2):
+            prob_index = numpy.nonzero(ds1-ds2)[0]
+            probs.extend(zip([ds_name]*len(prob_index),
+                             prob_index.tolist(),
+                             ds1[prob_index].tolist(),
+                             ds2[prob_index].tolist()))
         if i % progress_each == 0:
             logging.info("Progress: %4d /%4d", i, ds_count)
 
+    if probs:
+        print("Problematic datasets:\n")
+        format = "| %10s | %10s | %15s | %15s |"
+        print(format % ("Dataset", "Index", "Left val", "Right val"))
+        print("|" + '=' * 61 + "|")
+        for p in probs:
+            print(format % p)
+    else:
+        print("No differences found!")
 
-_doc = """
-Usage:
-  nrn_compare summary <file1> <file2>
-  nrn_compare -h
-
-Options:
-  -h                Show help
-  -vv               Verbose mode (-v for info, -vv for debug) 
-"""
 
 if __name__ == "__main__":
-    args = docopt(_doc)
+    args = docopt(__doc__)
     if args["summary"]:
         check_summary(args["<file1>"], args["<file2>"])
