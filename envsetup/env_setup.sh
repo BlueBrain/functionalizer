e5cb5f13d717392c974f91bda5695ec1106f3f49
Fernando Pereira <fernando.pereira@epfl.ch>
2018-04-04 14:20:27 +0200

    Update requirements and envscript
    Change-Id: I4fd893b10395e4c75160f7a120fec825c12c2c30

--- envsetup/env_setup.sh
+++ envsetup/env_setup.sh
@@ -1,12 +1,42 @@
-#!/usr/bin/env bash
-PROJ_DIR=$HOME/dev/Functionalizer
-CURDIR=$HOME/dev/Functionalizer/spykfunc
-. $HOME/dev/sparkenv/bin/activate
-export PYSPARK_DRIVER_PYTHON=ipython
+#!/usr/bin/bash -i
+###########################################
+# Prepare/Load a Python env to run spark
+##########################################
+
+set -e
+
+BASEDIR=$HOME/dev/Functionalizer/pyspark
+CURDIR=$BASEDIR/envsetup
+PYENV=$CURDIR/sparkenv
+export SPARK_HOME=/home/leite/usr/spark-2.2.1
+
+if [ ! -d $PYENV ]; then
+    echo "Creating virtualenv in $PYENV"
+    virtualenv $PYENV -p `which python`
+    . $PYENV/bin/activate
+    pip install --upgrade setuptools pip
+    pip install -e $SPARK_HOME/python  # Access spark via install develop
+    pip install -r $BASEDIR/dev-requirements.txt
+    # Build install spykfunc
+    rm $BASEDIR/build -rf
+    BUILD_TYPE=DEVEL pip install -e $BASEDIR[dev]
+else
+    . $PYENV/bin/activate
+fi
+
 export SPARK_CONF_DIR=$CURDIR/spark_conf
 
 # Set Spark paths
-export PYTHONPATH="/home/leite/usr/spark-2.2.0/python":$PYTHONPATH
-export PATH=/home/leite/usr/spark-2.2.1/bin:$PATH
+export PATH=$SPARK_HOME/bin:$PATH
 
-export SPARK_OPTS="$ss --conf spark.sql.shuffle.partitions=$((`nproc`*4)) --jars java/spykfunc_udfs.jar"
+# Options to pyspark for running spykfunc
+export SPYKFUNC_SPARK_OPTS="--conf spark.sql.shuffle.partitions=$((`nproc`*4)) --jars java/spykfunc_udfs.jar"
+
+function pyspark_ipython {
+  PYSPARK_DRIVER_PYTHON=ipython pyspark $SPYKFUNC_SPARK_OPTS
+}
+export -f pyspark_ipython
+
+# Start subshell
+bash
+
