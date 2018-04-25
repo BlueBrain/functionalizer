428a804eecdcc234d35c3ad2c1f864e9fdc8361f
Matthias Wolf <matthias.wolf@epfl.ch>
2018-04-25 09:37:42 +0200

    Snapshot of job analysis before refactoring.

--- tools/scaling.py
+++ tools/scaling.py
@@ -4,7 +4,6 @@
 from __future__ import print_function
 import argparse
 import datetime
-import itertools
 import json
 import logging
 import pandas
@@ -68,6 +67,9 @@ def extract_ganglia(metric, alias, hosts, start, end):
     for host in hosts:
         rck = rack.match(host).group(1)
         r = requests.get(get.format(rck, host, metric, start, end))
+        print(get.format(rck, host, metric, start, end))
+        print(r)
+        print(r.text)
         data = pandas.read_csv(StringIO(r.text))
         data.rename(columns=dict(zip(data.columns, ['timestamp'] + alias)), inplace=True)
         data['timestamp'] = pandas.to_datetime(data.timestamp, utc=False)
@@ -88,6 +90,8 @@ def process_response(response, columns, collapse=False):
     :param columns: the column names to use
     :param collapse: collapse data by building mean, min, and max columns
     """
+    print(response)
+    print(response.text)
     datas = []
     rawdata = json.loads(response.text)
     if collapse:
@@ -96,6 +100,8 @@ def process_response(response, columns, collapse=False):
         df = pandas.DataFrame(columns=[col, 'timestamp'], data=data['datapoints'])
         df['timestamp'] = pandas.to_datetime(df.timestamp, unit='s', utc=False)
         datas.append(df)
+        if collapse and 'cpu' in data['target']:
+            L.info("sum %s for %s: %f", columns[0], data['target'], df[col].sum())
     if collapse:
         data = pandas.concat(datas).groupby('timestamp').aggregate(['mean', 'min', 'max'])
         data.columns = ['_'.join([c, f.replace('mean', 'avg')]) for (c, f) in data.columns]
@@ -126,6 +132,7 @@ def extract_graphite(hosts, start, end):
 
     cpu_metric = 'target=sumSeries(bb5.ps.{}_bbp_epfl_ch.cpu.*.percent.active)'
     cpu_query = get.format('&'.join(cpu_metric.format(h) for h in hosts), start, end)
+    print(cpu_query)
     cpu_data = process_response(requests.get(cpu_query), ['cpu'], collapse=True)
 
     in_metric = 'target=perSecond(bb5.ps.{}_bbp_epfl_ch.interface.ib0.if_octets.rx)'
@@ -163,10 +170,28 @@ def expand_hosts(nodes):
     return subprocess.check_output('scontrol show hostname {}'.format(nodes).split()).split()
 
 
+def get_slurm_data(jobid):
+    """Returns the nodelist and requested cpus for a slurm job id.
+    """
+    def conv(t):
+        return datetime.datetime.strptime(t, '%Y-%m-%dT%H:%M:%S')
+    output = subprocess.check_output('sacct --noheader -P -o node,ntasks,ncpus,start,end -j {}'.format(jobid).split())
+    ntasks = max(line.split('|')[1] for line in output.splitlines())
+    nodes, _, cpus, start, end = output.splitlines()[0].split('|')
+    return nodes, int(ntasks), int(cpus), conv(start), conv(end)
+
+
 def extract_node_data(nodes, start, end):
     hosts = expand_hosts(nodes)
-    disk = extract_ganglia('m=part_max_used', ['disk'], hosts, start, end)
+    L.error(hosts)
     try:
+        disk = extract_ganglia('m=part_max_used', ['disk'], hosts, start, end)
+    except Exception:
+        disk = pandas.DataFrame(columns=["timestamp", "disk_avg", "disk_max", "disk_min"],
+                                data=[[start, 0, 0, 0], [end, 0, 0, 0]])
+        disk.set_index("timestamp", inplace=True)
+    try:
+        print(start, end)
         data = extract_graphite(hosts, start, end)
         disk = disk.append(pandas.DataFrame(index=data.index.copy())) \
                    .interpolate(method='nearest')
@@ -184,62 +209,110 @@ def extract_node_data(nodes, start, end):
     return data
 
 
+def extract_data_from_json(fn):
+    """Extract data from a JSON file that spykfunc writes along with the output
+    """
+    with open(fn, 'r') as fd:
+        data = json.load(fd)
+
+    # assert(len(data['timing']) == 1)
+    slurm = data.get('slurm', dict())
+
+    try:
+        nodes = len(expand_hosts(slurm['nodes']))
+        execs = data['spark']['executors']
+        ncores = data['spark']['parallelism']
+
+        circuit = os.path.basename(os.path.dirname(fn))
+        mode = None
+    except KeyError:
+        m = extract.search(fn)
+        if not m:
+            L.error("no match for pattern in %s", fn)
+            return None, None, None, None
+        circuit, mode = m.groups()[:2]
+        ncores, nodes, execs = (int(n) for n in m.groups()[2:])
+    size = ncores // execs
+    occupancy = ncores // nodes
+
+    timing = dict(data['timing'][-1])
+    rules = maybe(timing['filter_by_rules'])
+    cut = maybe(timing['run_reduce_and_cut'])
+    export = maybe(timing.get('export_results', [0]))
+    runtime = maybe(data.get('runtime', [[None]])[-1])
+    try:
+        start, end = (str(int(float(s))) for s in data['runtime'][-1][1:])
+    except Exception:
+        start = maybe(data.get('runtime', [[None]])[-1], idx=1)
+        end = None
+    version = data.get('version', data.get('spark', dict()).get('version'))
+    df = pandas.DataFrame(columns=COLUMNS,
+                          data=[[fn, (slurm or dict()).get('jobid'), circuit,
+                                 ncores, size, occupancy, (mode or ''),
+                                 version, rules, cut, export, runtime, start]])
+    return df, slurm.get('nodes'), start, end
+
+
+def extract_data_from_slurm(jobid, circuit, version='C', fn=None):
+    nodenames, tasks, ncores, start, end = get_slurm_data(jobid)
+    runtime = int((end - start).total_seconds())
+
+    nodes = len(expand_hosts(nodenames))
+    size = ncores // tasks
+    occupancy = ncores // nodes
+    rules = None
+    cut = None
+    export = None
+
+    def epic(t):
+        epoch = datetime.datetime.fromtimestamp(0)
+        return int((t - epoch - datetime.timedelta(hours=1)).total_seconds())
+
+    df = pandas.DataFrame(columns=COLUMNS,
+                          data=[[fn, jobid, circuit,
+                                 ncores, size, occupancy, '',
+                                 version, rules, cut, export, runtime, epic(start)]])
+    return df, nodenames, epic(start), epic(end)
+
+
 def extract_data(fns, timeline=False):
+    """Extract data from log files and monitoring.
+
+    Add information from monitoring if flag `timeline` is set.
+    """
     if isinstance(fns, basestring):
         fns = [fns]
     for fn in fns:
         L.info("processing %s", fn)
         try:
-            with open(fn, 'r') as fd:
-                data = json.load(fd)
-
-            # assert(len(data['timing']) == 1)
-            slurm = data.get('slurm')
-
-            try:
-                nodes = len(expand_hosts(slurm['nodes']))
-                execs = data['spark']['executors']
-                ncores = data['spark']['parallelism']
-
+            jobmatch = re.search(r'_(\d+).\w+$', fn)
+            if fn.endswith(".json"):
+                df, nodes, start, end = extract_data_from_json(fn)
+            elif jobmatch:
+                jobid = jobmatch.group(1)
                 circuit = os.path.basename(os.path.dirname(fn))
-                mode = None
-            except KeyError:
-                m = extract.search(fn)
-                if not m:
-                    L.error("no match for pattern in %s", fn)
-                    continue
-                circuit, mode = m.groups()[:2]
-                ncores, nodes, execs = (int(n) for n in m.groups()[2:])
-            size = ncores // execs
-            occupancy = ncores // nodes
+                df, nodes, start, end = extract_data_from_slurm(jobid, circuit, fn=fn)
+            else:
+                L.error("cannot process %s", fn)
+                continue
 
-            timing = dict(data['timing'][-1])
-            rules = maybe(timing['filter_by_rules'])
-            cut = maybe(timing['run_reduce_and_cut'])
-            export = maybe(timing.get('export_results', [0]))
-            runtime = maybe(data.get('runtime', [[None]])[-1])
-            start = maybe(data.get('runtime', [[None]])[-1], idx=1)
-            version = data.get('version', data.get('spark', dict()).get('version'))
-            df = pandas.DataFrame(columns=COLUMNS,
-                                  data=[[fn, (slurm or dict()).get('jobid'), circuit,
-                                         ncores, size, occupancy, (mode or ''),
-                                         version, rules, cut, export, runtime, start]])
+            if df is None:
+                continue
 
             if not timeline:
                 yield df, None
                 continue
 
-            if slurm is None:
+            if nodes is None:
                 L.error("no slurm data for %s", fn)
                 yield df, None
                 continue
 
-            start, end = (str(int(float(s))) for s in data['runtime'][-1][1:])
-            pickle = fn.replace(".json", ".pkl")
+            pickle = os.path.splitext(fn)[0] + ".pkl"
             if os.path.exists(pickle):
                 timedata = pandas.read_pickle(pickle)
             else:
-                timedata = extract_node_data(slurm['nodes'], start, end)
+                timedata = extract_node_data(nodes, start, end)
                 timedata.to_pickle(pickle, protocol=-1)
             yield df, timedata
         except Exception as e:
@@ -323,6 +396,9 @@ def annotate_plot(fig, info):
 
 
 def annotate_steps(ax, fn):
+    if not fn.endswith(".json"):
+        L.warn('no json data present, skipping annotations')
+        return
     steps = [("filter_by_rules", 1),
              ("run_reduce_and_cut", 1),
              ("apply_reduce", 0),
