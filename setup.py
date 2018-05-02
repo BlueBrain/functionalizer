64ceffab8c17baf6d349b4cba9383233e1094c2e
Matthias Wolf <matthias.wolf@epfl.ch>
2018-05-02 14:56:05 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 import os.path as osp
 import glob
 
-SPYKFUNC_VERSION = "0.7.1.dev1"
+SPYKFUNC_VERSION = "0.7.1.dev2"
 BUILD_TYPE = os.getenv('BUILD_TYPE', "RELEASE").upper()
 BASE_DIR = osp.dirname(__file__)
 EXAMPLES_DESTINATION = "share/spykfunc/examples"
