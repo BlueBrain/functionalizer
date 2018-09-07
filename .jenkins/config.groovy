acff755927f0cf661f77aad3ecb98ac37de4d53b
Matthias Wolf <matthias.wolf@epfl.ch>
2018-09-07 14:13:17 +0200

    Attempt to hide more changes from Jenkins logs.
    Change-Id: I254516d0fe5ba3fe7672ab4d0d611aeaefb1ad2c

--- .jenkins/config.groovy
+++ .jenkins/config.groovy
@@ -1,7 +1,8 @@
 node {
     library identifier: 'bbp@master', retriever: modernSCM(
         [$class:'GitSCMSource',
-         remote: 'ssh://bbpcode.epfl.ch/hpc/jenkins-pipeline'])
+         remote: 'ssh://bbpcode.epfl.ch/hpc/jenkins-pipeline',
+         changelog: false])
 
     nix("mod-spykfunc-dev",
         "ssh://bbpcode.epfl.ch/building/Spykfunc")
