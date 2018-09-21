abf5205a0a187686888a747876250df7bee86ee1
Matthias Wolf <matthias.wolf@epfl.ch>
2018-09-21 14:34:13 +0200

    Run python tests on prod at night, too!
    Also use /nvme for Spark testing.
    
    Change-Id: I77051cf8fab15aca13bbfa6f71e45170e4e3279f

--- .jenkins/test_pytest.sh
+++ .jenkins/test_pytest.sh
@@ -1,3 +1,5 @@
+script=$(mktemp)
+cat >$script <<EOF
 # Dependencies to build test environment
 module load nix/boost
 module load nix/hdf5
@@ -5,4 +7,14 @@ module load nix/hpc/highfive
 
 cd $WORKSPACE
 git submodule update --init --recursive
-srun -Aproj16 -pinteractive -Cnvme --exclusive --mem=0 python setup.py test
+export SPARK_LOCAL_DIRS=/nvme/\$(whoami)/\$SLURM_JOBID/spark-local
+export SPARK_WORKER_DIR=/nvme/\$(whoami)/\$SLURM_JOBID/spark-worker
+python setup.py test
+EOF
+
+part=$([ $(date +%H) -gt 8 -a $(date +%H) -lt 20 ] && echo interactive || echo prod)
+# srun here rather than salloc since the latter executes on the local
+# machine, with internal sruns running on remote nodes with requested
+# resources.  `sm_run` needs salloc, here we want to be on an /nvme node!
+srun -Aproj16 -p$part -Cnvme -N1 --exclusive --mem=0 \
+    sh $script
