b3c3e6f1d6089d9332534e07c0cc27ca83e594b2
Matthias Wolf <matthias.wolf@epfl.ch>
2018-10-08 11:07:58 +0200

    Account for larger range of morphologies.
    Change-Id: I4a27af6cb79c5eb21f4bce1463266ce9cf139c64

--- spykfunc/schema.py
+++ spykfunc/schema.py
@@ -25,7 +25,7 @@ NEURON_SCHEMA = T.StructType([
     T.StructField("id", T.IntegerType(), False),
     T.StructField("mtype_i", T.ShortType(), False),
     T.StructField("etype_i", T.ShortType(), False),
-    T.StructField("morphology_i", T.ShortType(), False),
+    T.StructField("morphology_i", T.IntegerType(), False),
     T.StructField("syn_class_i", T.ShortType(), False),
     T.StructField("layer", T.ShortType(), False),
     T.StructField("position", T.ArrayType(T.DoubleType(), False), False),
