0838966b0410f3a855bd1166af744d41e423e1b5
Matthias Wolf <matthias.wolf@epfl.ch>
2021-09-13 12:29:10 +0000

    refactor: simplify setup

--- setup.py
+++ setup.py
@@ -21,28 +21,12 @@ class CMakeExtension(Extension):
         self.sourcedir = os.path.abspath(sourcedir)
 
 
-def find_cmake():
-    for candidate in ["cmake", "cmake3"]:
-        try:
-            out = subprocess.check_output([candidate, "--version"])
-            cmake_version = LooseVersion(
-                re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-            )
-            if cmake_version >= "3.2.0":
-                return candidate
-        except OSError:
-            pass
-
-    raise RuntimeError("CMake >= 3.2.0 must be installed to build Spykfunc")
-
-
 class CMakeBuild(build_ext):
     def run(self):
-        cmake = find_cmake()
         for ext in self.extensions:
-            self.build_extension(ext, cmake)
+            self.build_extension(ext)
 
-    def build_extension(self, ext, cmake):
+    def build_extension(self, ext):
         source = Path(self.build_temp).resolve() / self.get_ext_filename(ext.name)
         extdir = str(source.parent)
         cmake_args = [
@@ -53,34 +37,15 @@ class CMakeBuild(build_ext):
         cfg = "Debug" if self.debug else "Release"
         build_args = ["--config", cfg]
 
-        if "BOOST_ROOT" in os.environ:
-            cmake_args += ["-DBOOST_ROOT={}".format(os.environ["BOOST_ROOT"])]
-
-        if platform.system() == "Windows":
-            cmake_args += [
-                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-            ]
-            if sys.maxsize > 2 ** 32:
-                cmake_args += ["-A", "x64"]
-            build_args += ["--", "/m"]
-        else:
-            cmake_args += ["-DCMAKE_BUILD_TYPE={}".format(cfg)]
-            build_args += ["--", "-j"]
-
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
-        try:
-            proc = subprocess.Popen("echo $CXX", shell=True, stdout=subprocess.PIPE)
-            output = subprocess.check_call(
-                [cmake, ext.sourcedir] + cmake_args, cwd=self.build_temp
-            )
-            output = subprocess.check_call(
-                [cmake, "--build", "."] + build_args, cwd=self.build_temp
-            )
-        except subprocess.CalledProcessError as exc:
-            print("Status : FAIL", exc.returncode, exc.output)
-            raise
+        output = subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        output = subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
 
         # Put into the local source directory
         target = Path(self.get_ext_fullpath(ext.name)).resolve()
